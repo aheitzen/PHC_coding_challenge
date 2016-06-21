3aafb23d814a9ed33f16ee3e0382d29652a84e2b
aheitzen <alexaheitzenrader@yahoo.com>
2016-06-21 12:36:09 -0700

    figuring out jQuery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 console.log("I work");
 
-function formValidation() {
-	var x = document.forms["myForm"]["form_name"].value;
-	if(x == null || x == "") {
-		alert("I AM ERROR");
-		return false;
-	}
-}
+$(document).ready(function(){
+	$("#submit_button").trigger(function(event){
+		event.preventDefault();
+    	alert("I AM ERROR");
+		return true;
+        
+    });
+
+
+$("#submit_button").submit(function() {
+	console.log("form")
+})
+});
+
+
+
+
+
+// function formValidation() {
+// 	var x = document.forms["myForm"]["form_name"].value;
+// 	if(x == null || x == "") {
+// 		alert("Whoa Tiger, did you fill out all the forms?");
+// 		return false;
+// 	}
+// }
