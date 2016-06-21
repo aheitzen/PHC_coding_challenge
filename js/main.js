919a4ad9193e04c9e4d15f7adec26cc445e0172f
aheitzen <alexaheitzenrader@yahoo.com>
2016-06-21 11:35:49 -0700

    added three more forms

--- js/main.js
+++ js/main.js
@@ -1,12 +1,2 @@
 console.log("I work");
 
-// Handlebars.registerPartial('myPartial', '{{name}}')
-
-// {{#> layout }}
-//   My Content
-// {{/layout}}
-
-
-$('#datetimepicker1').data("DateTimePicker").FUNCTION({
-
-});
