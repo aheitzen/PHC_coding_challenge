677a29d5b6af513cdc21a16d1ce94e42c341b250
aheitzen <alexaheitzenrader@yahoo.com>
2016-06-21 12:05:40 -0700

    adding form validation js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 console.log("I work");
 
+function formValidation() {
+	var x = document.forms["myForm"]["form_name"].value;
+	if(x == null || x == "") {
+		alert("I AM ERROR");
+		return false;
+	}
+}
