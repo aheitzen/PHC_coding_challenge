01942598e90340b57bd0990c49a49a4600287689
aheitzen <alexaheitzenrader@yahoo.com>
2016-06-22 16:06:47 -0700

    changing form layout

--- js/main.js
+++ js/main.js
@@ -3,8 +3,16 @@ var hasBeenClicked = false;
 function formValidation(event) {
 	if(hasBeenClicked == false) {
 		alert("I AM ERROR");
+		// console.log($('#myForm').val());
 		event.preventDefault();
 		hasBeenClicked = true;
-		console.log(event);
+		// console.log(event);
+
 	}
 }
+
+$( "form" ).submit(function( event ) {
+  console.log( $( this ).serializeArray() );
+  event.preventDefault();
+});
+
