7fee0b845f63917b8b9718dca790ee36d23f6269
aheitzen <alexaheitzenrader@yahoo.com>
2016-06-21 21:27:32 -0700

    form validation

--- js/main.js
+++ js/main.js
@@ -1,27 +1,10 @@
-console.log("I work");
+var hasBeenClicked = false;
 
-$(document).ready(function(){
-	$("#submit_button").trigger(function(event){
+function formValidation(event) {
+	if(hasBeenClicked == false) {
+		alert("I AM ERROR");
 		event.preventDefault();
-    	alert("I AM ERROR");
-		return true;
-        
-    });
-
-
-$("#submit_button").submit(function() {
-	console.log("form")
-})
-});
-
-
-
-
-
-// function formValidation() {
-// 	var x = document.forms["myForm"]["form_name"].value;
-// 	if(x == null || x == "") {
-// 		alert("Whoa Tiger, did you fill out all the forms?");
-// 		return false;
-// 	}
-// }
+		hasBeenClicked = true;
+		console.log(event);
+	}
+}
