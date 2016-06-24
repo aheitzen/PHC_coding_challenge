91f76f98dbd0994ea17767f85b76e1f9e03a7e56
aheitzen <alexaheitzenrader@yahoo.com>
2016-06-23 23:24:07 -0700

    stuff

--- js/main.js
+++ js/main.js
@@ -3,16 +3,19 @@ var hasBeenClicked = false;
 function formValidation(event) {
 	if(hasBeenClicked == false) {
 		alert("I AM ERROR");
-		// console.log($('#myForm').val());
 		event.preventDefault();
 		hasBeenClicked = true;
 		// console.log(event);
 
+	} else {
+		var values = {};
+		$.each($('#myForm').serializeArray(), function(i, field) {
+	    	values[field.name] = field.value;
+		});
+		console.log(values);
 	}
 }
 
-$( "form" ).submit(function( event ) {
-  console.log( $( this ).serializeArray() );
-  event.preventDefault();
-});
+
+
 
